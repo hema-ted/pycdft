3e78c2f5913e05b7b019e3bab852f4cb6c681c3f
mahe <ted.mahe@gmail.com>
2018-08-28 12:13:21 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="pycdft",
+    version="0.5",
+    author="He Ma, Wennie Wang, Siyoung Kim, Man Hin Cheng, Marco Govoni",
+    author_email="mahe@uchicago.edu",
+    description="",
+    packages=['pycdft'],
+    install_requires=[
+        "ase",
+        "numpy",
+    ],
+)
+
