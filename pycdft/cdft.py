a78def9b2508afd6a2b6af0e407fa13e9d62da55
wennie wang <wwwennie@gmail.com>
2019-03-27 19:03:46 -0500

    added compute_elcoupling_nonsymm and added more sigfigs for printout in cdft.py

--- pycdft/cdft.py
+++ pycdft/cdft.py
@@ -172,11 +172,11 @@ class CDFTSolver:
         print("  E (Ed + Ec) = {:.6f}".format(self.sample.Ed + self.sample.Ec))
         print("  W (free energy) = {:.6f}".format(self.sample.W))
         for i, c in enumerate(self.constraints):
-            print("  > Constraint #{} (type = {}, N0 = {}, V = {:.6f}):".format(
+            print("  > Constraint #{} (type = {}, N0 = {}, V = {:.12f}):".format(
                 i, c.type, c.N0, c.V
             ))
             print("    N = {:.6f}".format(c.N))
-            print("    dW/dV = N - N0 = {:.6f}".format(c.dW_by_dV))
+            print("    dW/dV = N - N0 = {:.8f}".format(c.dW_by_dV))
         print("Elapsed time: ")
         self.timer(start_dft,end_dft)
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
