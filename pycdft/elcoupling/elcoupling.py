39e7d8f44a5d2d160ac801ed2a0a1d13d8dd0bae
wennie wang <wwwennie@gmail.com>
2019-06-19 12:13:22 -0500

    added timer, added spin polarization; untested

--- pycdft/elcoupling/elcoupling.py
+++ pycdft/elcoupling/elcoupling.py
@@ -3,25 +3,26 @@ from scipy.linalg import fractional_matrix_power
 from pycdft.cdft import CDFTSolver
 from pycdft.common.ft import FFTGrid, ftrr
 from pycdft.common.units import hartree_to_ev, hartree_to_millihartree
+import time 
 
-def compute_elcoupling(solver1: CDFTSolver, solver2: CDFTSolver):
+def compute_elcoupling(solver1: CDFTSolver, solver2: CDFTSolveri,debug=True):
     """ Compute electronic coupling in mH between two KS wavefunctions.
    
         Compatible for symmetric and non-symmetric systems
         TODO: implementation of spin  
+        TODO?: more than one constraint
  
         Notes on implementation, see:
         1) Oberhofer & Blumberger 2010; dx.doi.org/10.1063/1.3507878
         2) Kaduk, et al 2012; dx.doi.org/10.1021/cr200148b, esp. p 344, Eq. 51 
         3) Goldey, et al 2017; dx.doi.org/10.1021/acs.jctc.7b00088
   
-        For Qbox, only @ Gamma point, so quantities are real; but keep conjugate operations for now
+        only @ Gamma point, so quantities are real; but keep conjugate operations for now
     """
-
+    start_time = time.time()
     assert solver1.sample.vspin == solver2.sample.vspin
     vspin = solver1.sample.vspin
    
-    # TODO: more than one constraint
     if vspin != 1:
         raise NotImplementedError
 
@@ -32,7 +33,6 @@ def compute_elcoupling(solver1: CDFTSolver, solver2: CDFTSolver):
     assert wfc1.nkpt == wfc2.nkpt
     assert np.all(wfc1.nbnd == wfc2.nbnd)
     nspin, nkpt, nbnd, norb = wfc1.nspin, wfc1.nkpt, wfc1.nbnd, wfc1.norb
-    print("npin,nkpt,nbnd,norb:", nspin,nkpt,nbnd,norb)
 
     # Gamma point only
     if nspin not in [1, 2] or nkpt != 1:
@@ -51,11 +51,11 @@ def compute_elcoupling(solver1: CDFTSolver, solver2: CDFTSolver):
     #----------------------------------------------
   
     # To calculate the coupling, we need S, W, and H
-    print(" See Oberhofer & Blumberger 2010: dx.doi.org/10.1063/1.3507878 ")
-    print(" Below is a breakdown of components that go into calculating H_ab" )
+    print("")
     # S matrix
     O = cdft_get_O(wfc1,wfc2,omega,m)
-    S = cdft_get_S(O,nspin)
+    S,Odet = cdft_get_S(O,nspin)
+    print("DONE: S\nElapsed time:", timer(start_time,time.time()))
 
     # W matrix 
     # TODO: averaging for Hermitian H in *get_H; remove here ?
@@ -63,33 +63,51 @@ def compute_elcoupling(solver1: CDFTSolver, solver2: CDFTSolver):
     # where V_i = \sum V w_j, i.e., the constraint lagrange multiplier is included 
     Vc_dense = 0.5 * (solver1.Vc_tot + solver2.Vc_tot)[0, ...]
     Vc = ftrr(Vc_dense, source=FFTGrid(n1, n2, n3), dest=FFTGrid(m1, m2, m3)).real
-    W = cdft_get_W(wfc1,wfc2,Vc,O,omega,m)
+    W,C = cdft_get_W(wfc1,wfc2,Vc,O,omega,m)
+    print("DONE: W\nElapsed time:", timer(start_time,time.time()))
     
     # H matrix 
-    H = cdft_get_H(solver1,solver2,S,W)
+    H,Stot = cdft_get_H(solver1,solver2,S,W)
+    print("DONE: H\nElapsed time:", timer(start_time,time.time()))
 
     # H matrix between orthogonal diabatic states 
-    Hsymm = cdft_get_Hsymm(H,S)
+    Hsymm = cdft_get_Hsymm(H,Stot)
  
+    # debug output
+    print("")
+    if debug:
+      print(""); print(" Below is a breakdown of components that go into calculating H_ab" )
+      print(" npin: %d, nkpt: %d \n nbnd (per spin channel): %s, norb: %d"%(nspin,nkpt,nbnd,norb))
+      for ispin in range(nspin):
+         print("~~~~~~~~~~~~~~~~~~ Spin Channel %d ~~~~~~~~~~~~~~~~"%ispin)
+         print("O matrix"); print(O[:,:,ispin]); print("|O|:",Odet[ispin]); print("")
+         print("S matrix"); print(S[:,:,ispin]); print("")
+         print("W matrix"); print(W[:,:,ispin]); print("")
+         print("--> Cofactor:"); print(C[:,:,ispin]); print("")
+         print("H matrix"); print(H); print("")
+         print("H ortho. Lowdin"); print(Hsymm); print("")
+
     # final output
+    print("~~~~~~~~~~~~~~~~~ Electronic Coupling ~~~~~~~~~~~~~~~~~~")
+    print(" See Oberhofer & Blumberger 2010: dx.doi.org/10.1063/1.3507878 ")
+    print("")
     print("|Hab| (H):", np.abs(Hsymm[0, 1]))
     print("|Hab| (mH):", np.abs(Hsymm[0, 1] * hartree_to_millihartree))
     print("|Hab| (eV):", np.abs(Hsymm[0, 1] * hartree_to_ev))
+    print(""); print("Elapsed time for Electronic Coupling:",timer(start_time,time.time()))
 
 def cdft_get_O(wfc1,wfc2,omega,m):
     """ Overlap matrix 
       For plane waves, see Eq. 20 in Oberhofer & Blumberger 2010"""
 
     nspin, nkpt, nbnd, norb = wfc1.nspin, wfc1.nkpt, wfc1.nbnd, wfc1.norb
-    O = np.zeros([norb, norb])
+    O = np.zeros([norb, norb,nspin])
     for ispin in range(nspin):
         for ibnd, jbnd in np.ndindex(nbnd[ispin, 0], nbnd[ispin, 0]):
             i = wfc1.skb2idx(ispin, 0, ibnd)
             j = wfc2.skb2idx(ispin, 0, jbnd) # before was wfc1 
-            O[i, j] = (omega / m) * np.sum(np.conjugate(wfc1.psi_r[i]) * wfc2.psi_r[j])
+            O[i, j,ispin] = (omega / m) * np.sum(np.conjugate(wfc1.psi_r[i]) * wfc2.psi_r[j])
  
-    print("O matrix:")
-    print(O)
     return O
 
 def cdft_get_S(O,nspin):
@@ -97,67 +115,101 @@ def cdft_get_S(O,nspin):
       O  is orbital overlap matrix 
  
       nspin = 1: S is 2x2 matrix
-      nspin = 2: S is 2x2x2 matrix (S_ij, spin channel)"""
+      nspin = 2: S is 2x2x2 matrix (S_ij, spin channel)
+ 
+      returns spin-polarized S, not Stot = Sup x Sdown
+    """
 
-    Odet = np.linalg.det(O)
-    print("|O|:", Odet)
 
-    # 2x2 state overlap matrix S
-    S = np.eye(2)
-    S[1,0] = Odet # S_BA
-    S[0,1] = np.conjugate(Odet) # Eq. 12, Oberhofer2010 # S_AB
-    print("S matrix containing (plane wave) orbital overlaps:")
-    print(S)
+    Odet = np.zeros(nspin)
+    S = np.zeros([2,2,nspin]) # 2x2(x2 for spin) state overlap matrix S
+    for ispin in range(nspin):
+       Odet[ispin] = np.linalg.det(O[:,:,ispin])
    
-    return S
+       S[0,0,ispin] = S[1,1,ispin] = 1.0d0
+       S[1,0,ispin] = Odet[ispin] # S_BA
+       S[0,1,ispin] = np.conjugate(Odet[ispin]) # Eq. 12, Oberhofer2010 # S_AB
+ 
+    return S, Odet
 
 def cdft_get_W(wfc1,wfc2,Vc,O,omega,m): 
-    """ build W matrix """
+    """ build W matrix 
+        returns spin-polarized W, not Wtot = (Sdown x Wup) + (Sup x Wdown)
+    """
 
     nspin, nkpt, nbnd, norb = wfc1.nspin, wfc1.nkpt, wfc1.nbnd, wfc1.norb
+    C = np.zeros([norb,norb,nspin]); CT = np.zeros([norb,norb,nspin])
+    Odet = np.zeros(nspin); Oinv = np.zeros(nspin)
+    P12 = np.zeros([norb, norb,nspin]); 
+    P11 = np.zerpos([norb,norb,nspin]); P22 = np.zeros([norb,norb,nspin])
+    W = np.zeros([2,2,nspin])
 
-    # see Eq. 25 in Oberhofer2010
-    # cofactor matrix C
-    Odet = np.linalg.det(O)
-    Oinv = np.linalg.inv(O)
-    CT = Oinv @ (Odet*np.eye(norb))
-    C = CT.T
-
-    # constraint potential matrix P, for finding V_a*W_ab, see Eq. 22 in Oberhofer2010
-    # in our notation: Vab = V_a*W_ab
-    P = np.zeros([norb, norb])
     for ispin in range(nspin):
-        for ibnd, jbnd in np.ndindex(nbnd[ispin, 0], nbnd[ispin, 0]):
-            i = wfc1.skb2idx(ispin, 0, ibnd)
-            j = wfc2.skb2idx(ispin, 0, jbnd)
-            p = (omega / m) * np.sum(np.conjugate(wfc1.psi_r[i]) * Vc * wfc2.psi_r[j])
-            # print(p)
-            P[i, j] = p # orbital overlaps, <\phi_B | w | \phi_a>
-    print("P (<phi_m|w(r)|phi_n>):", P)
-    print("C (cofactor matrix):", C)
-    Vab = np.trace(P @ C) # \sum_ij = Tr(A_ij * B_ij) 
-    Vba = np.conjugate(Vab)
+       # see Eq. 25 in Oberhofer2010
+       # cofactor matrix C
+       Odet[ispin]= np.linalg.det(O[:,:,ispin])
+       Oinv[ispin] = np.linalg.inv(O[:,:,ispin)
+       CT[:,:,ispin] = Oinv[ispin] @ (Odet[ispin]*np.eye(norb))
+       C[:,:,ispin] = CT[:,:,ispin].T
+   
+       # constraint potential matrix P, for finding V_a*W_ab, see Eq. 22 in Oberhofer2010
+       # in our notation: Vab = V_a*W_ab
+       for ibnd, jbnd in np.ndindex(nbnd[ispin, 0], nbnd[ispin, 0]):
+           i = wfc1.skb2idx(ispin, 0, ibnd)
+           j = wfc2.skb2idx(ispin, 0, jbnd)
 
-    W = np.zeros([2,2])
-    # TODO: set W[0,0], W[1,1]
-    W[0,1] = Vab
-    W [1,0] = Vba
+           p = (omega / m) * np.sum(np.conjugate(wfc1.psi_r[i]) * Vc * wfc2.psi_r[j])
+           P12[i, j,ispin] = p # orbital overlaps, <\phi_A | w | \phi_B>
 
-    return W
+           p = (omega / m) * np.sum(np.conjugate(wfc2.psi_r[i]) * Vc * wfc1.psi_r[j])
+           P21[i, j,ispin] = p # orbital overlaps, <\phi_B | w | \phi_A>
+
+           # nspin = 1 doesn't need on-diagonal W, omit for speed up?
+           # would require some code restructuring
+           p = (omega / m) * np.sum(np.conjugate(wfc1.psi_r[i]) * Vc * wfc1.psi_r[j])
+           P11[i, j,ispin] = p # orbital overlaps, <\phi_A | w | \phi_A>
+
+           p = (omega / m) * np.sum(np.conjugate(wfc2.psi_r[i]) * Vc * wfc2.psi_r[j])
+           P22[i, j,ispin] = p # orbital overlaps, <\phi_B | w | \phi_B>
+      
+       Vab = np.trace(P12[:,:,ispin] @ C[:,:,ispin]) # \sum_ij = Tr(A_ij * B_ij) 
+       Vba = np.trace(P21[:,:,ispin] @ C[:,:,ispin])
+
+       Vaa = np.trace(P11[:,:,ispin] @ C[:,:,ispin])
+       Vbb = np.trace(P22[:,:,ispin] @ C[:,:,ispin]) 
+   
+       W[0,0,ispin] = Vaa
+       W[1,1,ispin] = Vbb
+       W[0,1,ispin] = Vab
+       W[1,0,ispin] = Vba
+
+    return W, C
 
 def cdft_get_H(solver1,solver2,S,W):
     """ 
      H matrix between nonorthogonal diabatic states 
      (Eq. 9-11 in Oberhofer2010, Eq. 5 in Goldey2017)
      see also p 344 of Kaduk2012
-    
-     H_aa = <\psi_a|H_KS|\psi_a>
-     H_ab = F_b * S_ab - V_b * W_ab
-     H_ba = F_a * S_ba - V_a * W_ba
 
-     F_a = <\psi_a|H_KS + V*w | \psi_a>
+     nspin = 1    
+       H_aa = <\psi_a|H_KS|\psi_a>
+       H_ab = F_b * S_ab - V_b * W_ab
+       H_ba = F_a * S_ba - V_a * W_ba
+
+       F_a = <\psi_a|H_KS + V*w | \psi_a>
+
+     nspin = 2 -> requires additional calc of 
+                     Stot = Sup x Sdown
+                     Wtot = (Sup x Wdown) + (Sdown x Wup)
     """ 
-
+    if nspin == 1:
+      Stot = S[:,:,0]
+      Wtot = W[:,:,0]
+    else:
+      # all element-wise operations
+      Stot = np.multiply(S[:,:,0],S[:,:,1])
+      Wtot = np.add(np.multiply(S[:,:,1],W[:,:,0]), np.multiply(S[:,:,0],W[:,:,1]))
+   
     H = np.zeros([2, 2])
     H[0, 0] = solver1.sample.Ed
     H[1, 1] = solver2.sample.Ed
@@ -166,12 +218,10 @@ def cdft_get_H(solver1,solver2,S,W):
 
     # to make H hermitian
     # H_ab -> 1/2(H_ab + H_ba)
-    H[0, 1] = 0.5 * (Fb * S[0, 1] + Fa * S[1,0]) - 0.5 * (W[0,1]+W[1,0])
+    H[0, 1] = 0.5 * (Fb * Stot[0, 1] + Fa * Stot[1,0]) - 0.5 * (Wtot[0,1]+Wtot[1,0])
     H[1,0] = np.conjugate(H[0,1])
-    print("H matrix between nonorthogonal diabatic states:")
-    print(H)
    
-    return H
+    return H,Stot
 
 def cdft_get_Hsymm(H,S):
     """ 
@@ -183,3 +233,8 @@ def cdft_get_Hsymm(H,S):
     print(Hsymm)
   
     return Hsymm
+
+def timer(start,end):
+        hours, rem = divmod(end-start,3600)
+        minutes, seconds = divmod(rem, 60)
+        print("{:0>2}h:{:0>2}m:{:05.2f}s".format(int(hours),int(minutes),seconds))
