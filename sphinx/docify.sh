ffca450d42fa1face24ec354736eb0ef235b9dc1
wennie wang <wwwennie@gmail.com>
2020-01-16 10:15:10 -0600

    minor file edits in sphinx

--- sphinx/docify.sh
+++ sphinx/docify.sh
@@ -7,10 +7,10 @@
 #   pip install sphinx_rtd_theme --user
 #   
 
+# CHANGE THIS
 home_dir="/home/wwwennie/bin2/pycdft/sphinx/"
 
 cd $home_dir
 rm -r $home_dir/build/*
 
 sphinx-build -b html $home_dir/source/ $home_dir/build/
-scp -r $home_dir/build  wwwennie@205.208.85.116:~/Downloads/
