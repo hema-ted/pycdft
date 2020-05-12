b73a45ebd5f742908626f610a91ce58f8b376efe
mahe <ted.mahe@gmail.com>
2020-05-11 23:24:25 -0500

    version 1.0

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup
 
-# make sure PYTHONPATH contains the path of a directory you can write to
-# e.g., in this instance
-#    export PYTHONPATH="${PYTHONPATH}:/path/to/pycdft/lib/python3.6/site-packages"
-#    python setup.py install --prefix="/path/to/pycdft/"
-# the "/lib/python3.6/site-packages/" gets tacked on later
-
 setup(
     name="pycdft",
-    version="0.9",
+    version="1.0",
     author="He Ma, Wennie Wang, Siyoung Kim, Man Hin Cheng, Marco Govoni, Giulia Galli",
     author_email="mahe@uchicago.edu",
-    description="",
     packages=['pycdft'],
     install_requires=[
         "ase",
@@ -21,4 +14,3 @@ setup(
         "lxml"
     ],
 )
-
